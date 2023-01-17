8a41bb2df5c0edb9b537cd0d7127c9431a10ff7e
anondev2323 <anondev2323@gmail.com>
2023-01-17 14:06:53 -0500

    add: advanced dropdown

--- src/views/TokensModal.tsx
+++ src/views/TokensModal.tsx
@@ -1,11 +1,13 @@
 import { makeStyles } from '@mui/styles';
 import React from 'react';
+import Collapse from '@mui/material/Collapse';
 import Header from '../components/Header';
 import Modal from '../components/Modal';
 import Spacer from '../components/Spacer';
 import Search from '../components/Search';
 import Scroll from '../components/Scroll';
 import { Theme } from '@mui/material';
+import ArrowDownIcon from '../icons/arrow-down.svg';
 
 import { MAINNET_TOKENS } from '../sdk/config/MAINNET';
 import { OPACITY } from '../utils/style';
@@ -48,6 +50,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   tokenRowRight: {
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
   },
   tokenRowBalanceText: {
     opacity: '60%',
@@ -64,10 +67,28 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  advanced: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '16px',
+    cursor: 'pointer',
+  },
+  arrow: {
+    transition: 'transform 0.4s',
+  },
+  invert: {
+    transform: 'rotate(180deg)',
+  }
 }));
 
 function TokensModal() {
   const classes = useStyles();
+  const [showAdvanced, setShowAdvanced] = React.useState(false);
+  const toggleAdvanced = () => {
+    setShowAdvanced((prev) => !prev);
+  };
+
   return (
     <Modal closable width={500}>
       <Header text="Select token" />
@@ -97,19 +118,13 @@ function TokensModal() {
               </div>
             );
           })}
-          {/* // .filter((c) => !!c.icon)
-            // .map((chain, i) => {
-            //   return (
-            //     <div key={i} className={classes.networkTile}>
-            //       <img
-            //         src={chain.icon}
-            //         alt={chain.displayName}
-            //         className={classes.networkIcon}
-            //       />
-            //       <div className={classes.networkText}>{chain.displayName}</div>
-            //     </div>
-            //   );
-            // })} */}
+          <div className={classes.advanced} onClick={toggleAdvanced}>
+            <div className={classes.subheader}>Advanced</div>
+            <img className={`${classes.arrow} ${showAdvanced && classes.invert}`} src={ArrowDownIcon} alt="down arrow" />
+          </div>
+          <Collapse in={showAdvanced}>
+            <div>Advanced Options</div>
+          </Collapse>
         </div>
       </Scroll>
     </Modal>
