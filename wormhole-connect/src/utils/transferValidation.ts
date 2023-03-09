d5d0e9cf4d8e374088b1f71c38764c3c8f47b231
anondev2323 <anondev2323@gmail.com>
2023-03-09 10:57:46 -0500

    refactor: use npm instead of yarn

--- wormhole-connect/src/utils/transferValidation.ts
+++ wormhole-connect/src/utils/transferValidation.ts
@@ -161,7 +161,6 @@ export const validateAll = (
 
 export const isTransferValid = (validations: TransferValidations) => {
   for (const validationErr of Object.values(validations)) {
-    console.log(validationErr);
     if (!!validationErr) return false;
   }
   return true;
