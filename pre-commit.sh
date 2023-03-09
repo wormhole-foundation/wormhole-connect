6b1fc1c34bce2a406f728e541e49eaff52aec568
anondev2323 <anondev2323@gmail.com>
2023-03-09 11:44:21 -0500

    refactor: pre-commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+
+# Stash uncommitted changes
+git stash push --keep-index
+
+abort()
+{
+  echo_abort
+  echo "An error occurred. Please review your code and try again" >&2
+  git stash pop
+  exit 1
+}
+
+trap 'abort' 0
+
+set -e
+git update-index -q --refresh
+
+# Do things here
+# ====================================
+
+# lint
 npm run lint
+git add .
+
+# ====================================
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE ***
+************
+'
+
+git stash pop -q
