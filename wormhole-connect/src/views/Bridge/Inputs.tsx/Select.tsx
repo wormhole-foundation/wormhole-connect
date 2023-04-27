1ebf23e80c6a4a3dcff769b4b751c39b4f4ef177
anondev2323 <anondev2323@gmail.com>
2023-04-27 11:25:32 -0400

    fix: disable opening token modal until wallet and network are selected

--- wormhole-connect/src/views/Bridge/Inputs.tsx/Select.tsx
+++ wormhole-connect/src/views/Bridge/Inputs.tsx/Select.tsx
@@ -22,24 +22,33 @@ type Props = {
   selected: Selected | undefined;
   error?: boolean;
   editable?: boolean;
+  disabled?: boolean;
   onClick?: any;
 };
 
 function Select(props: Props) {
   const { classes } = useStyles();
 
+  const handleClick = () => {
+    if (props.editable && !props.disabled) {
+      props.onClick();
+    }
+  };
+
   return (
     <Input
       label={props.label}
       error={props.error}
       editable={props.editable}
-      onClick={props.onClick}
+      onClick={handleClick}
     >
       {props.selected ? (
         <div className={classes.select}>
           <TokenIcon name={props.selected.icon} height={24} />
           {props.selected.text}
         </div>
+      ) : props.disabled ? (
+        '-'
       ) : (
         <div className={classes.select}>
           <TokenIcon height={24} />
