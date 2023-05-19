7d93e3c8d6075f262e99b542173d56bfab58d36c
anondev2323 <anondev2323@gmail.com>
2023-05-19 11:03:29 -0400

    refactor: make modals more modular

--- wormhole-connect/src/store/transfer.ts
+++ wormhole-connect/src/store/transfer.ts
@@ -32,6 +32,7 @@ export interface TransferState {
   toNetwork: ChainName | undefined;
   automaticRelayAvail: boolean;
   token: string;
+  destToken: string;
   amount: number | undefined;
   destGasPayment: PaymentOption;
   maxSwapAmt: number | undefined;
@@ -68,6 +69,7 @@ const initialState: TransferState = {
   toNetwork: config?.bridgeDefaults?.toNetwork || undefined,
   automaticRelayAvail: false,
   token: config?.bridgeDefaults?.token || '',
+  destToken: '',
   amount: undefined,
   destGasPayment: PaymentOption.MANUAL,
   maxSwapAmt: undefined,
@@ -106,6 +108,12 @@ export const transferSlice = createSlice({
     // user input
     setToken: (state: TransferState, { payload }: PayloadAction<string>) => {
       state.token = payload;
+    },
+    setDestToken: (
+      state: TransferState,
+      { payload }: PayloadAction<string>,
+    ) => {
+      state.destToken = payload;
     },
     setFromNetwork: (
       state: TransferState,
@@ -243,6 +251,7 @@ export const {
   touchValidations,
   setValidations,
   setToken,
+  setDestToken,
   setFromNetwork,
   setToNetwork,
   setDestGasPayment,
