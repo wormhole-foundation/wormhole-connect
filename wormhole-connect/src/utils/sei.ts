1cec82e0a33e10130dfe72bb364c47bdddc0d0ec
M-Picco <martin@xlabs.xyz>
2023-05-20 19:23:47 -0300

    sei: remove hardcoded address contract and use config instead

--- wormhole-connect/src/utils/sei.ts
+++ wormhole-connect/src/utils/sei.ts
@@ -1,13 +1,18 @@
 import { ChainName } from '@wormhole-foundation/wormhole-connect-sdk';
+import { WH_CONFIG } from '../config';
 
 export function buildSeiPayload(
   toNetwork: ChainName,
   recipient: string,
 ): { receiver?: string; payload?: Uint8Array } {
   if (toNetwork === 'sei') {
+    const translatorAddress =
+      WH_CONFIG.chains.sei?.contracts.seiTokenTranslator;
+    if (!translatorAddress)
+      throw new Error('Sei token translator not configured not found');
+
     return {
-      receiver:
-        'sei1dkdwdvknx0qav5cp5kw68mkn3r99m3svkyjfvkztwh97dv2lm0ksj6xrak',
+      receiver: translatorAddress,
       payload: new Uint8Array(
         Buffer.from(
           JSON.stringify({
