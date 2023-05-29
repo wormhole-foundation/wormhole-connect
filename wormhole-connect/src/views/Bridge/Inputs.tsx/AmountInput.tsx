0b6a1104583b141aa9a024f8820de4015feb9c2a
anondev2323 <anondev2323@gmail.com>
2023-05-29 10:43:45 -0400

    fix: amount input logic

--- wormhole-connect/src/views/Bridge/Inputs.tsx/AmountInput.tsx
+++ wormhole-connect/src/views/Bridge/Inputs.tsx/AmountInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import { setAmount } from '../../../store/transfer';
@@ -6,26 +6,66 @@ import { validate } from '../../../utils/transferValidation';
 
 import InputTransparent from '../../../components/InputTransparent';
 import Input from './Input';
+import { toFixedDecimals } from '../../../utils/balance';
 
 function AmountInput() {
   const dispatch = useDispatch();
+  const amountEl = useRef(null);
   const {
     validate: showErrors,
     validations,
     token,
     isTransactionInProgress,
+    amount,
   } = useSelector((state: RootState) => state.transfer);
+  const [value, setValue] = useState(amount ? `${amount}` : '');
 
   function handleAmountChange(event) {
-    const newAmount = Number.parseFloat(event.target.value);
-    dispatch(setAmount(newAmount));
+    let value = event.target.value;
+    const index = value.indexOf('.');
+    switch (true) {
+      case index === -1: {
+        value = Number.parseInt(event.target.value);
+        break;
+      }
+      case index === 0: {
+        value = '0.';
+        break;
+      }
+      case index >= 0 && index < value.length - 1: {
+        value = toFixedDecimals(value, 8);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    setValue(value);
+    dispatch(setAmount(Number.parseFloat(value)));
   }
   const validateAmount = () => validate(dispatch);
 
+  useEffect(() => {
+    setValue(amount ? `${amount}` : '');
+  }, [amount]);
+
+  const focus = () => {
+    if (amountEl.current) {
+      (amountEl.current as any).focus();
+    }
+  };
+
   return (
-    <Input label="Amount" error={!!(showErrors && validations.amount)} editable>
+    <Input
+      label="Amount"
+      error={!!(showErrors && validations.amount)}
+      editable
+      onClick={focus}
+    >
       {token ? (
         <InputTransparent
+          inputRef={amountEl}
           placeholder="0.00"
           type="number"
           min={0}
@@ -33,6 +73,7 @@ function AmountInput() {
           onChange={handleAmountChange}
           onPause={validateAmount}
           disabled={isTransactionInProgress}
+          value={value}
         />
       ) : (
         <div>-</div>
