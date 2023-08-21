b78b69be52f89d645a6e6f24fbe5a8c693eb39cf
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-08-21 11:25:10 -0500

    remove console logs

--- wormhole-connect/src/views/Redeem/SendTo.tsx
+++ wormhole-connect/src/views/Redeem/SendTo.tsx
@@ -131,7 +131,6 @@ function SendTo() {
       : claimError
       ? 'Error please retry . . .'
       : 'Claim below';
-  console.log(`IS CONNECTED ${isConnected} WALLET ${wallet.address}`);
   return (
     <div>
       <InputContainer>
