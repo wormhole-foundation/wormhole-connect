aeec83bb461581b32908a4475b7508b4b1ef0a8e
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-08-30 16:39:49 -0400

    disable cctp

--- wormhole-connect/src/utils/routes/cctpManual.ts
+++ wormhole-connect/src/utils/routes/cctpManual.ts
@@ -219,6 +219,8 @@ export class CCTPManualRoute extends BaseRoute {
     sourceChain: ChainName | ChainId,
     destChain: ChainName | ChainId,
   ): Promise<boolean> {
+    return false;
+
     const sourceTokenConfig = TOKENS[sourceToken];
     const destTokenConfig = TOKENS[destToken];
 
