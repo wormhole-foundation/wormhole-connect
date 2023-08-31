6215c543f83e95a3204d26db7632e9904cf77de4
M-Picco <martin@xlabs.xyz>
2023-08-30 21:45:15 -0300

    Remove obsolete wormhole host

--- sdk/src/config/MAINNET.ts
+++ sdk/src/config/MAINNET.ts
@@ -235,7 +235,6 @@ const MAINNET_CONFIG: WormholeConfig = {
   chains: MAINNET,
   wormholeHosts: [
     'https://wormhole-v2-mainnet-api.certus.one',
-    'https://wormhole.inotel.ro',
     'https://wormhole-v2-mainnet-api.mcf.rocks',
     'https://wormhole-v2-mainnet-api.chainlayer.network',
     'https://wormhole-v2-mainnet-api.staking.fund',
