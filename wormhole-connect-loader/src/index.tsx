17d7f58041777ca22b6006cf48f82b9359c9f94f
Kevin Peters <kevin@w7.xyz>
2023-08-30 21:00:38 -0500

    fixed formatting

--- wormhole-connect-loader/src/index.tsx
+++ wormhole-connect-loader/src/index.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { WormholeConnectConfig } from "./types";
+import React from 'react';
+import { WormholeConnectConfig } from './types';
 
-class WormholeBridge extends React.Component<{
-  config?: WormholeConnectConfig;
-}> {
+class WormholeBridge extends React.Component<
+  { config?: WormholeConnectConfig; }
+> {
   componentDidMount() {
     const script = document.createElement("script");
-    script.src =
-      "https://www.unpkg.com/@wormhole-foundation/wormhole-connect@0.0.10/dist/main.js";
+    script.src = "https://www.unpkg.com/@wormhole-foundation/wormhole-connect@0.0.10/dist/main.js";
     script.async = true;
 
     const link = document.createElement("link");
-    link.href =
-      "https://www.unpkg.com/@wormhole-foundation/wormhole-connect@0.0.10/dist/main.css";
+    link.href = "https://www.unpkg.com/@wormhole-foundation/wormhole-connect@0.0.10/dist/main.css";
 
     document.body.appendChild(script);
     document.body.appendChild(link);
@@ -21,14 +19,11 @@ class WormholeBridge extends React.Component<{
   render() {
     return (
       // @ts-ignore
-      <div
-        id="wormhole-connect"
-        config={this.props.config ? JSON.stringify(this.props.config) : null}
-      ></div>
+      <div id="wormhole-connect" config={this.props.config ? JSON.stringify(this.props.config) : null}></div>
     );
   }
 }
 
-export * from "./theme";
-export * from "./types";
+export * from './theme';
+export * from './types';
 export default WormholeBridge;
