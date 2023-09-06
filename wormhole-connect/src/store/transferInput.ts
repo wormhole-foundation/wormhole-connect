94a16cd5a08b034dacf84c1ca92825104da30310
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-09-06 17:13:38 -0400

    Revert "Logic for showing correct USDC"
    This reverts commit 86e4e4255e42a1b0971f3574946a48b83835b07b.

--- wormhole-connect/src/store/transferInput.ts
+++ wormhole-connect/src/store/transferInput.ts
@@ -31,17 +31,6 @@ export const formatBalance = (
   return { [token.key]: formattedBalance };
 };
 
-// for use in USDC or other tokens that have versions on many chains
-// returns token key
-export const getNativeVersionOfToken = (
-  tokenSymbol: string,
-  chain: ChainName,
-): string =>
-  Object.entries(TOKENS)
-    .map(([key, t]) => t)
-    .find((t) => t.symbol === tokenSymbol && t.nativeNetwork === chain)?.key ||
-  '';
-
 export type ValidationErr = string;
 
 export type TransferValidations = {
@@ -167,12 +156,6 @@ export const transferInputSlice = createSlice({
           state.token = '';
           state.amount = '';
         }
-        if (
-          tokenConfig.symbol === 'USDC' &&
-          tokenConfig.nativeNetwork !== fromNetwork
-        ) {
-          state.token = getNativeVersionOfToken('USDC', fromNetwork);
-        }
       }
     },
     setToNetwork: (
@@ -180,17 +163,6 @@ export const transferInputSlice = createSlice({
       { payload }: PayloadAction<ChainName>,
     ) => {
       state.toNetwork = payload;
-      const { toNetwork, destToken } = state;
-
-      if (destToken) {
-        const tokenConfig = TOKENS[destToken];
-        if (
-          tokenConfig.symbol === 'USDC' &&
-          tokenConfig.nativeNetwork !== toNetwork
-        ) {
-          state.destToken = getNativeVersionOfToken('USDC', toNetwork);
-        }
-      }
     },
     setAmount: (
       state: TransferInputState,
