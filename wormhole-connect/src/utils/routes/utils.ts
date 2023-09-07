315a8ae43ea8f33b2786397322d6eacbb0167580
Anon dev <anondev2323@gmail.com>
2023-09-07 10:54:00 -0400

    fix: format gas units correctly

--- wormhole-connect/src/utils/routes/utils.ts
+++ wormhole-connect/src/utils/routes/utils.ts
@@ -4,7 +4,11 @@ import {
   parseTokenTransferPayload,
   parseVaa,
 } from '@certusone/wormhole-sdk';
-import { ChainName, ChainId } from '@wormhole-foundation/wormhole-connect-sdk';
+import {
+  ChainName,
+  ChainId,
+  Context,
+} from '@wormhole-foundation/wormhole-connect-sdk';
 import { CHAINS } from 'config';
 import { Route } from 'config/types';
 import { BigNumber, utils } from 'ethers';
@@ -34,6 +38,10 @@ export const getRouteForVaa = (vaa: SignedVaa): Route => {
 
 export const formatGasFee = (chain: ChainName | ChainId, gasFee: BigNumber) => {
   const chainName = wh.toChainName(chain);
-  const nativeDecimals = CHAINS[chainName]?.nativeTokenDecimals;
+  const chainConfig = CHAINS[chainName]!;
+  const nativeDecimals = chainConfig.nativeTokenDecimals;
+  if (chainConfig.context === Context.ETH) {
+    return toFixedDecimals(utils.formatUnits(gasFee, 9), 6);
+  }
   return toFixedDecimals(utils.formatUnits(gasFee, nativeDecimals), 6);
 };
