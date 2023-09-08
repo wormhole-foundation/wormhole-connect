ca1cdce29c33f1f16808b66bdc21a18266880417
Evan Gray <battledingo@gmail.com>
2023-09-08 12:07:09 -0400

    builder: initial config

--- builder/src/index.tsx
+++ builder/src/index.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: { mode: "dark" },
+    components: {
+      MuiButton: { styleOverrides: { root: { textTransform: "none" } } },
+    },
+  })
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
