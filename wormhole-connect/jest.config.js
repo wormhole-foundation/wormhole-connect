da6e418ed2ef5e033be5553ca900b6c2130c63c7
Evan Gray <battledingo@gmail.com>
2023-10-09 20:43:57 -0400

    ci: jest replacement for explicit esm import

--- wormhole-connect/jest.config.js
+++ wormhole-connect/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
-  }
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@certusone/wormhole-sdk/lib/esm': '@certusone/wormhole-sdk/lib/cjs',
+  },
 };
