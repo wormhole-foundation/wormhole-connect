3ef84d67499d1ccc72fbbfe1087981e031c56315
Martin Picco <martin@xlabs.xyz>
2023-11-14 18:17:19 -0300

    [Bugfix] Signing with ledger on Osmosis (#1202)
    * bump aggregator sdk cosmos packages version
    
    * Increase IBC transfer timeout to 1 day

--- wormhole-connect/src/routes/cosmosGateway/utils/consts.ts
+++ wormhole-connect/src/routes/cosmosGateway/utils/consts.ts
@@ -1,5 +1,5 @@
 export const IBC_MSG_TYPE = '/ibc.applications.transfer.v1.MsgTransfer';
 export const IBC_PORT = 'transfer';
-export const IBC_TIMEOUT_MILLIS = 10 * 60 * 1000; // 10 minutes
+export const IBC_TIMEOUT_MILLIS = 24 * 60 * 60 * 1000; // 1 day
 
 export const millisToNano = (seconds: number) => seconds * 1_000_000;
