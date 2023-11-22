28be8346d39fd5db8159df89892554d9452a4243
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-11-22 09:49:40 -0300

    remove unecesary padding

--- wormhole-connect/src/components/PageHeader.tsx
+++ wormhole-connect/src/components/PageHeader.tsx
@@ -7,38 +7,40 @@ import Header from './Header';
 import MenuFull from './MenuFull';
 import DownIcon from 'icons/Down';
 
-const useStyles = makeStyles()((theme) => ({
-  container: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '8px',
-    marginBottom: '40px',
-    [theme.breakpoints.down('sm')]: {
-      marginBottom: '20px',
+const useStyles = makeStyles<{ showHamburgerMenu: boolean }>()(
+  (theme, { showHamburgerMenu }) => ({
+    container: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '8px',
+      marginBottom: showHamburgerMenu ? '40px' : '0px',
+      [theme.breakpoints.down('sm')]: {
+        marginBottom: '20px',
+      },
     },
-  },
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-  },
-  left: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  arrowBack: {
-    transform: 'rotate(90deg)',
-    marginRight: '16px',
-    cursor: 'pointer',
-  },
-  description: {
-    fontWeight: '300',
-    fontSize: '14px',
-    opacity: '0.6',
-  },
-}));
+    header: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '100%',
+    },
+    left: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    arrowBack: {
+      transform: 'rotate(90deg)',
+      marginRight: '16px',
+      cursor: 'pointer',
+    },
+    description: {
+      fontWeight: '300',
+      fontSize: '14px',
+      opacity: '0.6',
+    },
+  }),
+);
 
 type PageHeaderProps = {
   title: string;
@@ -53,7 +55,7 @@ function PageHeader({
   description,
   showHamburgerMenu = true,
 }: PageHeaderProps) {
-  const { classes } = useStyles();
+  const { classes } = useStyles({ showHamburgerMenu });
   const dispatch = useDispatch();
   function goBack() {
     dispatch(setRoute('bridge'));
