9345bdfddc1d27a61734b9d87cd4aa6d176ef48a
Artur Sapek <art@wormholelabs.xyz>
2023-12-18 13:32:59 -0500

    fix entry points in loader

--- wormhole-connect/vite.config.ts
+++ wormhole-connect/vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig({
     },
   },
   build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/main.tsx'),
+      name: 'WormholeConnect',
+      fileName: 'wormhole-connect',
+    },
     outDir: './build',
   },
   plugins: [
