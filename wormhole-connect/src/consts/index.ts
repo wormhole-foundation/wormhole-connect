7250495146ad3c0d3af9f5f5d9be961f2f954306
yuli-ferna <35125931+yuli-ferna@users.noreply.github.com>
2024-02-27 15:11:00 -0300

    Add warning for transfer larger than CCTP max transfer limit (1M)  (#1682)

--- wormhole-connect/src/consts/index.ts
+++ wormhole-connect/src/consts/index.ts
@@ -1,2 +1,4 @@
 export const GOVERNOR_WHITEPAPER_URL =
   'https://github.com/wormhole-foundation/wormhole/blob/main/whitepapers/0007_governor.md';
+
+export const CCTP_MAX_TRANSFER_LIMIT = 1000000;
