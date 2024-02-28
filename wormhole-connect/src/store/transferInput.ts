b0566d4db90066400503a073a287e09a74b0da76
Kevin Peters <kevin@w7.xyz>
2024-02-28 15:55:53 -0600

    swap chains ntt support

--- wormhole-connect/src/store/transferInput.ts
+++ wormhole-connect/src/store/transferInput.ts
@@ -199,6 +199,8 @@ const performModificationsIfFromChainChanged = (state: TransferInputState) => {
       if (tokenConfig.nativeChain !== fromChain) {
         state.token = getNativeVersionOfToken(tokenConfig.symbol, fromChain!);
       }
+    } else if (tokenConfig.ntt && tokenConfig.nativeChain !== fromChain) {
+      state.token = getNativeVersionOfToken(tokenConfig.symbol, fromChain!);
     }
   }
 };
@@ -225,6 +227,8 @@ const performModificationsIfToChainChanged = (state: TransferInputState) => {
       if (tokenConfig.nativeChain !== toChain) {
         state.destToken = getNativeVersionOfToken(tokenConfig.symbol, toChain!);
       }
+    } else if (tokenConfig.ntt && tokenConfig.nativeChain !== toChain) {
+      state.destToken = getNativeVersionOfToken(tokenConfig.symbol, toChain!);
     }
   }
 };
