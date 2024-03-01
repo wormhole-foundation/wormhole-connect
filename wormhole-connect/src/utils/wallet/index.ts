50d67050c3d4ee006b709f670c3a5fcf322c25c3
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2024-03-01 16:29:01 -0600

    Fix cosmos wallet import issue (#1743)

--- wormhole-connect/src/utils/wallet/index.ts
+++ wormhole-connect/src/utils/wallet/index.ts
@@ -224,14 +224,18 @@ export const getWalletOptions = async (
     const seiOptions = await seiWallet.fetchOptions();
     return Object.values(mapWallets(seiOptions, Context.SEI));
   } else if (config.context === Context.COSMOS) {
-    const {
-      wallets: { cosmos, cosmosEvm },
-    } = await import('utils/wallet/cosmos');
-
-    if (config.id === CHAIN_ID_EVMOS) {
-      return Object.values(mapWallets(cosmos, Context.COSMOS, ['OKX Wallet']));
+    if (config.id !== CHAIN_ID_EVMOS) {
+      const {
+        wallets: { cosmos },
+      } = await import('utils/wallet/cosmos');
+      return Object.values(mapWallets(cosmos, Context.COSMOS));
     } else {
-      return Object.values(mapWallets(cosmosEvm, Context.COSMOS));
+      const {
+        wallets: { cosmosEvm },
+      } = await import('utils/wallet/cosmos');
+      return Object.values(
+        mapWallets(cosmosEvm, Context.COSMOS, ['OKX Wallet']),
+      );
     }
   }
   return [];
