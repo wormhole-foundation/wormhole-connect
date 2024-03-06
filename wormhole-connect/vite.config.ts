0b5dd31412ebd2463e572b9b924982bb59c1f5a8
Artur Sapek <art@wormholelabs.xyz>
2024-03-06 14:36:40 -0500

    export types

--- wormhole-connect/vite.config.ts
+++ wormhole-connect/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import dts from 'vite-plugin-dts';
 import path from 'path';
 
 // There are three configs this file can return.
@@ -29,6 +30,9 @@ const resolve = {
 const plugins = [
   checker({
     typescript: true,
+  }),
+  dts({
+    insertTypesEntry: true,
   }),
   react(),
   nodePolyfills({
