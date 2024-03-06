9e12af60052d5650ff670aa7ab4da5640cc810e1
Artur Sapek <art@wormholelabs.xyz>
2024-03-06 16:16:55 -0500

    fix type exports

--- wormhole-connect/src/index.ts
+++ wormhole-connect/src/index.ts
@@ -1,4 +1,8 @@
 import WormholeConnect from './WormholeConnect';
-
 export default WormholeConnect;
+
 export * from './theme';
+
+import type { WormholeConnectConfig } from './config/types';
+import type { ChainName } from '@wormhole-foundation/wormhole-connect-sdk';
+export { WormholeConnectConfig, ChainName };
