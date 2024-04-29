4a854aae72c0cd30c351fdb2aa7d3c0eb3907a73
Artur Sapek <art@wormholelabs.xyz>
2024-04-29 11:59:31 -0400

    token conversions... wip

--- wormhole-connect/src/config/converter.ts
+++ wormhole-connect/src/config/converter.ts
@@ -1,11 +1,17 @@
 import * as v1 from '@wormhole-foundation/wormhole-connect-sdk';
-import { Network as NetworkConnect } from 'config/types';
+import {
+  Network as NetworkConnect,
+  TokenConfig as TokenConfigV1,
+} from 'config/types';
+
+import config from 'config';
 
 import * as v2 from '@wormhole-foundation/sdk';
 
 import * as utils from 'utils/sdk';
 
 // SDKConverter provides utility functions for converting core types between SDKv1 and SDKv2
+// This is only meant to be used while we transition to SDKv2
 export class SDKConverter {
   wh: v1.WormholeContext;
 
@@ -13,17 +19,22 @@ export class SDKConverter {
     this.wh = wh;
   }
 
+  // Chain conversion
+
   toChainIdV1(chain: v2.Chain) {
     return v2.toChainId(chain) as v1.ChainId;
   }
 
   toChainNameV1(chain: v2.Chain) {
+    console.log('chain', chain);
     return this.wh.toChainName(this.toChainIdV1(chain));
   }
 
   toChainV2(chain: v1.ChainName | v1.ChainId): v2.Chain {
     return v2.toChain(utils.toChainId(chain));
   }
+
+  // Network conversion
 
   toNetworkV2(network: NetworkConnect): v2.Network {
     switch (network) {
@@ -38,5 +49,52 @@ export class SDKConverter {
 
   toNetworkV1(network: v2.Network): NetworkConnect {
     return network.toLowerCase() as NetworkConnect;
+  }
+
+  // Token conversion
+
+  toTokenIdV1(token: v2.TokenId): v1.TokenId | undefined {
+    if (token.address === 'native') {
+      // In Connect's legacy code, native tokens don't have a tokenId
+      return undefined;
+    } else {
+      return {
+        chain: this.toChainNameV1(token.chain),
+        address: token.address.toString(),
+      };
+    }
+  }
+
+  toTokenIdV2(token: v1.TokenId | TokenConfigV1): v2.TokenId {
+    if (this.isTokenConfigV1(token)) {
+      return v2.Wormhole.tokenId(
+        this.toChainV2(token.nativeChain),
+        token.tokenId?.address ?? 'native',
+      );
+    } else {
+      return v2.Wormhole.tokenId(this.toChainV2(token.chain), token.address);
+    }
+  }
+
+  isTokenConfigV1(v: v1.TokenId | TokenConfigV1): v is TokenConfigV1 {
+    return v.hasOwnProperty('key');
+  }
+
+  // Attempts to find the Connect TokenConfig, which is comomnly used in Connect code base,
+  // given a v2.TokenId
+  findTokenConfigV1(tokenId: v2.TokenId): TokenConfigV1 | undefined {
+    const isNative = tokenId.address === 'native';
+    const chain = this.toChainNameV1(tokenId.chain);
+
+    for (const token of config.tokensArr) {
+      if (token.nativeChain === chain) {
+        if (isNative && token.tokenId === undefined) {
+          // Connect's TokenConfig lacks a tokenId field when it's the native gas token
+          return token;
+        } else if (token.tokenId?.address === tokenId.address.toString()) {
+          return token;
+        }
+      }
+    }
   }
 }
