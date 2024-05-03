32014b990c6258862efc0b3ae1071d66248852af
Artur Sapek <art@wormholelabs.xyz>
2024-05-03 13:17:45 -0400

    remove `builder` (now in separate repo) (#2035)
    * remove builder (now in separate repo)
    
    * fix ci error (?)
    
    * ok

--- builder/scripts/token_keys.ts
+++
@@ -1,7 +0,0 @@
-import { TESTNET_TOKENS } from "../../wormhole-connect/src/config/testnet/tokens";
-import { MAINNET_TOKENS } from "../../wormhole-connect/src/config/mainnet/tokens";
-
-console.log("TESTNET TOKENS");
-console.log(Object.keys(TESTNET_TOKENS));
-console.log("MAINNET TOKENS");
-console.log(Object.keys(MAINNET_TOKENS));
