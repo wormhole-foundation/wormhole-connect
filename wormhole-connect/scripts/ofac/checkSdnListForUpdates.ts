9528157d3b0f50b604a6949262e06854eab77f70
Artur Sapek <art@wormholelabs.xyz>
2024-05-07 09:14:52 -0400

    automatically generate src/consts/wallet.ts

--- wormhole-connect/scripts/ofac/checkSdnListForUpdates.ts
+++ wormhole-connect/scripts/ofac/checkSdnListForUpdates.ts
@@ -1,5 +1,7 @@
 import { SANCTIONED_WALLETS } from '../../src/consts/wallet';
 import { getSanctionedAddresses } from './utils';
+import * as prettier from 'prettier';
+import fs from 'fs';
 
 const checkSdnListForUpdates = async () => {
   console.log('Checking OFAC SDN list for updates...');
@@ -8,10 +10,26 @@ const checkSdnListForUpdates = async () => {
     (address) => !SANCTIONED_WALLETS.has(address),
   );
 
+  // Always update file to ensure it's correctly formatted
+  const sourceCode = `export const SANCTIONED_WALLETS = Object.freeze(new Set(${JSON.stringify(
+    addresses,
+    null,
+    4,
+  )}));`;
+
+  try {
+    fs.writeFileSync(
+      'src/consts/wallet.ts',
+      await prettier.format(sourceCode, { parser: 'babel', singleQuote: true }),
+    );
+  } catch (e) {
+    // Throw an error so this script exits with a non-zero code, so it fails in CI
+    throw new Error(`Failed to update src/consts/wallet.ts: ${e}`);
+  }
+
   if (newAddresses.length) {
-    console.log('Updated list:', JSON.stringify(addresses, null, 2));
     throw new Error(
-      'New addresses found, please update `SANCTIONED_WALLETS` in `src/consts/wallet.ts`.',
+      'New addresses found. Updated `SANCTIONED_WALLETS` in `src/consts/wallet.ts`. Please commit and push changes.',
     );
   }
 
