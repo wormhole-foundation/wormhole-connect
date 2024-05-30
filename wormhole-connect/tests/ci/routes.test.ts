830c2cdcf9f557464c2c4cbb986684006820a44f
Artur Sapek <art@wormholelabs.xyz>
2024-05-29 20:13:57 -0400

    for some reason this fails in CI lol

--- wormhole-connect/tests/ci/routes.test.ts
+++ wormhole-connect/tests/ci/routes.test.ts
@@ -19,8 +19,8 @@ describe('supported routes', () => {
     // Token bridge
     ['ETH', 'WETH', 'ethereum', 'bsc', [Route.Bridge, Route.Relay]],
     ['DAI', 'DAI', 'ethereum', 'polygon', [Route.Bridge, Route.Relay]],
-    ['WSOL', 'WSOL', 'solana', 'ethereum', [Route.Bridge, Route.Relay]],
-    ['WSOL', 'WSOL', 'ethereum', 'solana', [Route.Bridge, Route.Relay]],
+    ['WSOL', 'WSOL', 'solana', 'ethereum', [Route.Bridge]],
+    ['WSOL', 'WSOL', 'ethereum', 'solana', [Route.Bridge]],
     ['ETH', 'WETH', 'ethereum', 'avalanche', [Route.Bridge, Route.Relay]],
     ['BNB', 'WBNB', 'bsc', 'polygon', [Route.Bridge, Route.Relay]],
     ['FTM', 'WFTM', 'fantom', 'polygon', [Route.Bridge, Route.Relay]],
