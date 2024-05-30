15b970d3f990e04fd139a5eaf0413d3023bbd63b
Artur Sapek <art@wormholelabs.xyz>
2024-05-29 20:00:48 -0400

    unit tests covering every route isRouteSupported

--- wormhole-connect/tests/ci/routes.test.ts
+++ wormhole-connect/tests/ci/routes.test.ts
@@ -1,55 +1,89 @@
 import { ChainId, ChainName } from '@wormhole-foundation/wormhole-connect-sdk';
-import RouteAbstract from 'routes/abstracts/routeAbstract';
-import { ETHBridge } from 'routes/porticoBridge/ethBridge';
-
 import { describe, expect, test } from 'vitest';
-
 import { setConfig } from 'config';
-import { Route } from 'config/types';
-import { is } from '@mysten/sui.js';
+import { Route, WormholeConnectConfig } from 'config/types';
+import { getRouteImpls } from 'routes/mappings';
 
 describe('supported routes', () => {
-  interface testCase {
-    sourceToken: string;
-    destToken: string;
-    sourceChain: ChainName | ChainId;
-    destChain: ChainName | ChainId;
-    route: Route;
-  }
+  type testCase = [
+    sourceToken: string,
+    destToken: string,
+    sourceChain: ChainName | ChainId,
+    destChain: ChainName | ChainId,
+    route: Route[],
+  ];
 
   setConfig({ env: 'mainnet' });
 
   const testCases: testCase[] = [
-    // Portico bridge
-    {
-      sourceToken: 'ETH',
-      destToken: 'WETHbsc',
-      sourceChain: 'ethereum',
-      destChain: 'bsc',
-      route: ETHBridge,
-    },
-    {
-      sourceToken: 'ETH',
-      destToken: 'WETHpolygon',
-      sourceChain: 'ethereum',
-      destChain: 'polygon',
-      route: ETHBridge,
-    },
+    // Token bridge
+    ['ETH', 'WETH', 'ethereum', 'bsc', [Route.Bridge, Route.Relay]],
+    ['DAI', 'DAI', 'ethereum', 'polygon', [Route.Bridge, Route.Relay]],
+    ['SOL', 'WSOL', 'solana', 'ethereum', [Route.Bridge, Route.Relay]],
+    ['WSOL', 'WSOL', 'ethereum', 'solana', [Route.Bridge, Route.Relay]],
+    ['ETH', 'WETH', 'ethereum', 'avalanche', [Route.Bridge, Route.Relay]],
+    ['BNB', 'WBNB', 'bsc', 'polygon', [Route.Bridge, Route.Relay]],
+    ['FTM', 'WFTM', 'fantom', 'polygon', [Route.Bridge, Route.Relay]],
+    ['CELO', 'CELO', 'celo', 'polygon', [Route.Bridge, Route.Relay]],
+    // ETH bridge
+    ['ETH', 'WETHbsc', 'ethereum', 'bsc', [Route.ETHBridge]],
+    ['ETH', 'WETHpolygon', 'ethereum', 'polygon', [Route.ETHBridge]],
+    // wstETHBridge
+    ['wstETH', 'wstETHpolygon', 'ethereum', 'polygon', [Route.wstETHBridge]],
+    ['wstETH', 'wstETHarbitrum', 'ethereum', 'arbitrum', [Route.wstETHBridge]],
+    // NTT
+    [
+      'USDCeth',
+      'USDCfantom',
+      'ethereum',
+      'fantom',
+      [Route.NttManual, Route.NttRelay],
+    ],
+    // CCTP
+    [
+      'USDCeth',
+      'USDCarbitrum',
+      'ethereum',
+      'arbitrum',
+      [Route.CCTPManual, Route.CCTPRelay],
+    ],
+    [
+      'USDCarbitrum',
+      'USDCpolygon',
+      'arbitrum',
+      'polygon',
+      [Route.CCTPManual, Route.CCTPRelay],
+    ],
+    // TBTC
+    ['tBTC', 'tBTCpolygon', 'ethereum', 'polygon', [Route.TBTC]],
+    ['tBTCoptimism', 'tBTC', 'optimism', 'ethereum', [Route.TBTC]],
+    // Cosmos Gateway
+    ['CELO', 'CELO', 'osmosis', 'celo', [Route.CosmosGateway]],
+    ['CELO', 'CELO', 'osmosis', 'moonbeam', [Route.CosmosGateway]],
+    ['GLMR', 'WGLMR', 'moonbeam', 'kujira', [Route.CosmosGateway]],
   ];
 
-  for (let tc of testCases) {
-    test(`${tc.route} ${tc.sourceChain}:${tc.sourceToken} -> ${tc.destChain}${tc.destToken}`, async () => {
-      const r = new tc.route();
+  for (let [
+    sourceToken,
+    destToken,
+    sourceChain,
+    destChain,
+    routes,
+  ] of testCases) {
+    for (let route of routes) {
+      test(`${route} ${sourceChain}:${sourceToken} -> ${destChain}${destToken}`, async () => {
+        const r = getRouteImpls(route).v1;
 
-      const isSupported = await r.isRouteSupported(
-        tc.sourceToken,
-        tc.destToken,
-        '1.0',
-        tc.sourceChain,
-        tc.destChain,
-      );
+        const isSupported = await r.isRouteSupported(
+          sourceToken,
+          destToken,
+          '1.0',
+          sourceChain,
+          destChain,
+        );
 
-      expect(isSupported).toBeTruthy();
-    });
+        expect(isSupported).toBeTruthy();
+      });
+    }
   }
 });
