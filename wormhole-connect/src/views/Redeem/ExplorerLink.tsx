f1b3ebcd98bd97120d167a7a2856d49a64a584e7
evgeniko <97796468+evgeniko@users.noreply.github.com>
2024-07-01 23:45:30 +0200

    replace deprecated sui explorer link with suivision (#2228)

--- wormhole-connect/src/views/Redeem/ExplorerLink.tsx
+++ wormhole-connect/src/views/Redeem/ExplorerLink.tsx
@@ -28,7 +28,7 @@ function ExplorerLink(props: ExplorerLinkProps) {
 
   const chainConfig = config.chains[props.chain]!;
 
-  let explorerLink;
+  let explorerLink = '';
   if (props.type === 'tx') {
     // TODO: refactor and use a map instead
     if (chainConfig.key === 'sui') {
@@ -64,11 +64,16 @@ function ExplorerLink(props: ExplorerLinkProps) {
       explorerLink += '?cluster=devnet';
     }
     if (chainConfig.key === 'sui') {
-      explorerLink += '?network=testnet';
+      explorerLink = explorerLink.replace('https://', 'https://testnet.');
     }
     if (chainConfig.key === 'aptos') {
       explorerLink += '?network=testnet';
     }
+  }
+
+  // ensure explorerLink is defined before returning the JSX
+  if (!explorerLink) {
+    return null;
   }
 
   return (
