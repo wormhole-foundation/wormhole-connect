d9ff771a36dae3692c8facfe94999e80f896834a
Artur Sapek <art@wormholelabs.xyz>
2024-07-17 10:38:24 -0400

    test

--- wormhole-connect/src/index.ts
+++ wormhole-connect/src/index.ts
@@ -1,11 +1,3 @@
-import '@wormhole-foundation/sdk/addresses';
-import '@wormhole-foundation/sdk/platforms/evm';
-import '@wormhole-foundation/sdk/platforms/solana';
-import '@wormhole-foundation/sdk/platforms/aptos';
-import '@wormhole-foundation/sdk/platforms/sui';
-import '@wormhole-foundation/sdk/platforms/cosmwasm';
-import '@wormhole-foundation/sdk/platforms/algorand';
-
 import WormholeConnect from './WormholeConnect';
 
 import type {
