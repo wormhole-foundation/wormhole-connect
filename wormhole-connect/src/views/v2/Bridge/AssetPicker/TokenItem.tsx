aaaf5707d4efda343825b53a30fffd1ebd799c37
Kevin Peters <kevin@w7.xyz>
2024-08-24 09:43:07 -0500

    TokenPicker should show token addresses for selected chain
    Previously the TokenPicker would always display the address of the token
    for its native chain even when the native chain was not selected.
    
    Now we show the foreign address when the selected chain != token.chain.

--- wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenItem.tsx
+++ wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenItem.tsx
@@ -13,6 +13,9 @@ import TokenIcon from 'icons/TokenIcons';
 import { ChainConfig, TokenConfig } from 'config/types';
 import config from 'config';
 
+import type { Chain } from '@wormhole-foundation/sdk';
+import { getWrappedToken } from 'utils';
+
 const useStyles = makeStyles()(() => ({
   tokenListItem: {
     display: 'flex',
@@ -34,6 +37,7 @@ const useStyles = makeStyles()(() => ({
 
 type TokenItemProps = {
   token: TokenConfig;
+  chain: Chain;
   disabled?: boolean;
   onClick: () => void;
   balance?: string;
@@ -44,10 +48,17 @@ function TokenItem(props: TokenItemProps) {
   const { classes } = useStyles();
   const theme = useTheme();
 
-  const chainConfig: ChainConfig | undefined =
-    config.chains[props.token.tokenId?.chain ?? ''];
-  const address = props.token.tokenId?.address;
-  const explorerURL = `${chainConfig?.explorerUrl}address/${address}`;
+  const { chain, token } = props;
+  const chainConfig: ChainConfig | undefined = config.chains[chain];
+  // If the token is native to the chain, show the token's address.
+  // Otherwise, show the wrapped token's address.
+  const address =
+    chain === token.nativeChain
+      ? token.tokenId?.address
+      : getWrappedToken(token)?.foreignAssets?.[chain]?.address;
+  const explorerURL = address
+    ? `${chainConfig?.explorerUrl}address/${address}`
+    : undefined;
   const addressDisplay = `${address?.slice(0, 4)}...${address?.slice(-4)}`;
 
   const displayName = props.token.displayName ?? props.token.symbol;
