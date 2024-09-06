443ba3cdb5834a84ea6a9bf22c10c56c8a34621b
Armen Nikoyan <armen@nikoyan.com>
2024-09-06 19:15:44 +0400

    fix(redesign): Tx complete icon swap (#2540)

---
+++ wormhole-connect/src/icons/TxComplete.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createSvgIcon } from '@mui/material';
+
+const TxCompleteIcon = createSvgIcon(
+  <svg
+    width="107"
+    height="106"
+    viewBox="0 0 107 106"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M53.2079 105.5C82.2486 105.5 105.791 81.9949 105.791 53C105.791 24.0051 82.2486 0.5 53.2079 0.5C24.1672 0.5 0.625 24.0051 0.625 53C0.625 81.9949 24.1672 105.5 53.2079 105.5ZM53.207 102.097C80.3654 102.097 102.382 80.1157 102.382 53.0001C102.382 25.8844 80.3654 3.90283 53.207 3.90283C26.0485 3.90283 4.03223 25.8844 4.03223 53.0001C4.03223 80.1157 26.0485 102.097 53.207 102.097Z"
+      fill="#C1BBF6"
+    />
+    <path
+      d="M46.8789 68L32.628906 53.75L36.19141 50.1875L46.8789 60.875L69.8164 37.9375L73.3789 41.5L46.8789 68Z"
+      fill="#C1BBF6"
+    />
+  </svg>,
+  'Alert',
+);
+
+export default TxCompleteIcon;
