b244b60b765d43d9a34b367814f6aedaabf8b297
Emre Bogazliyanlioglu <emre@wormholelabs.xyz>
2024-09-18 15:59:28 +0300

    [Redesign] Adding countdown and progress bar
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>

--- wormhole-connect/src/views/v2/TxHistory/Widget/Item.tsx
+++ wormhole-connect/src/views/v2/TxHistory/Widget/Item.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { useTheme } from '@mui/material';
+import { useTimer } from 'react-timer-hook';
+import { CircularProgress, useTheme } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
+import LinearProgress from '@mui/material/LinearProgress';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { makeStyles } from 'tss-react/mui';
@@ -12,7 +14,7 @@ import config from 'config';
 import { LOCAL_STORAGE_TXS } from 'config/constants';
 import ArrowRight from 'icons/ArrowRight';
 import TokenIcon from 'icons/TokenIcons';
-import { millisToHumanString } from 'utils';
+import { minutesAndSecondsWithPadding } from 'utils/transferValidation';
 import { toFixedDecimals } from 'utils/balance';
 
 import type { TransactionLocal } from 'config/types';
@@ -39,6 +41,8 @@ type Props = {
 const WidgetItem = (props: Props) => {
   const theme = useTheme();
 
+  const [senderTimestamp, setSenderTimestamp] = useState('');
+  const [etaExpired, setEtaExpired] = useState(false);
   const [inProgress, setInProgress] = useState(true);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -55,11 +59,21 @@ const WidgetItem = (props: Props) => {
     tokenKey,
   } = transaction;
 
+  // Initialize the countdown
+  const { seconds, minutes, isRunning, restart } = useTimer({
+    expiryTimestamp: new Date(),
+    autoStart: false,
+    onExpire: () => setEtaExpired(true),
+  });
+
+  // Check whether the current transaction is from Mayan API
+  // We need this to parse the API response accordingly
   const isMayanTx = useMemo(
-    () => explorerInfo.name === 'Mayan Explorer',
-    [explorerInfo.name],
+    () => explorerInfo.apiUrl.startsWith(config.mayanApi),
+    [explorerInfo.apiUrl],
   );
 
+  // Side-effect to poll the transaction info from the respective API
   useEffect(() => {
     if (!transaction) {
       return;
@@ -72,24 +86,28 @@ const WidgetItem = (props: Props) => {
 
       try {
         const res = await fetch(explorerInfo.apiUrl);
-
         const resPayload = await res.json();
 
         if (!cancelled) {
           let txInProgress = true;
+          let txSenderTimestamp = '';
           if (isMayanTx) {
             txInProgress =
-              resPayload?.clientStatus?.toLowerCase() !== 'completed';
+              resPayload?.clientStatus?.toLowerCase() !== 'completed' &&
+              resPayload?.clientStatus?.toLowerCase() !== 'refunded';
+            txSenderTimestamp = resPayload?.initiatedAt;
           } else {
             const txData = resPayload?.operations?.[0];
             if (txData) {
               txInProgress =
                 txData.sourceChain?.status?.toLowerCase() === 'confirmed' &&
                 txData.targetChain?.status?.toLowerCase() !== 'completed';
+              txSenderTimestamp = txData.sourceChain?.timestamp;
             }
           }
 
           setInProgress(txInProgress);
+          setSenderTimestamp(txSenderTimestamp);
         }
       } catch (error) {
         if (!cancelled) {
@@ -117,6 +135,75 @@ const WidgetItem = (props: Props) => {
     }
   }, [inProgress, txHash]);
 
+  // Sender time in milliseconds
+  const senderTime = useMemo(() => {
+    if (!senderTimestamp) {
+      return 0;
+    }
+
+    return new Date(senderTimestamp).getTime();
+  }, [senderTimestamp]);
+
+  // Remaining from the original ETA since the creation of this transaction
+  const etaRemaining = useMemo(() => {
+    if (!senderTime) {
+      // We need the sender timestamp to be able to calculate the remaining time
+      // Otherwise do not render the remaining eta counter
+      return 0;
+    }
+
+    const timePassed = Date.now() - senderTime;
+
+    if (eta < timePassed) {
+      return 0;
+    }
+
+    return eta - timePassed;
+  }, [senderTime, eta]);
+
+  // Displays the countdown
+  const etaCountdown = useMemo(() => {
+    if (!senderTime && isFetching) {
+      return <CircularProgress size={16} />;
+    }
+
+    if (etaExpired) {
+      return 'Any time now!';
+    }
+
+    const counter = isRunning
+      ? minutesAndSecondsWithPadding(minutes, seconds)
+      : null;
+
+    return counter;
+  }, [etaExpired, isRunning, minutes, seconds, senderTime, isFetching]);
+
+  // A number value between 0-100
+  const progressBarValue = useMemo(() => {
+    // etaRemaining is guaranteed to be smaller than or equal to eta,
+    // but we still check here as well to be on the safe side.
+    if (etaRemaining > eta) {
+      return 0;
+    }
+
+    // Return full bar if already completed
+    if (!inProgress) {
+      return 100;
+    }
+
+    return ((eta - etaRemaining) / eta) * 100;
+  }, [eta, etaRemaining, inProgress]);
+
+  // Start the timer when we have the sender timestamp to compute the remaining time
+  useEffect(() => {
+    if (isRunning || !etaRemaining) {
+      return;
+    }
+
+    // Start only when we have the remaining eta and if the timer hasn't been started yet
+    restart(new Date(Date.now() + etaRemaining), true);
+  }, [etaRemaining]);
+
   if (!transaction) {
     return <></>;
   }
@@ -137,16 +224,15 @@ const WidgetItem = (props: Props) => {
               justifyContent="space-between"
             >
               <Typography
-                fontSize={14}
                 justifyContent="space-between"
                 color={
                   inProgress
-                    ? theme.palette.text.secondary
+                    ? theme.palette.text.primary
                     : theme.palette.success.main
                 }
                 display="flex"
               >
-                {inProgress ? millisToHumanString(eta) : 'Completed'}
+                {inProgress ? etaCountdown : 'Completed'}
               </Typography>
               <Stack direction="row" alignItems="center">
                 <Typography fontSize={14} marginRight="8px">
@@ -162,6 +248,16 @@ const WidgetItem = (props: Props) => {
                 <TokenIcon icon={config.chains[destChain]?.icon} height={24} />
               </Stack>
             </Stack>
+            {inProgress && (
+              <LinearProgress
+                sx={{
+                  borderRadius: '4px',
+                  marginTop: '8px',
+                }}
+                variant="determinate"
+                value={progressBarValue}
+              />
+            )}
           </CardContent>
         </CardActionArea>
       </Card>
