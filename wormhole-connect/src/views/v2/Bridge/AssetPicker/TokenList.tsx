f596f3d18283c0e9d2c79a4aa476817525fc97bb
Kevin Peters <kevin@w7.xyz>
2024-09-24 08:44:24 -0500

    add comments, missed search case

--- wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenList.tsx
+++ wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenList.tsx
@@ -94,6 +94,7 @@ const TokenList = (props: Props) => {
       }
 
       // Exclude wormhole-wrapped tokens with no balance
+      // unless it's canonical
       if (
         props.isSource &&
         isWrappedToken(tokenConfig, selectedChainConfig.key) &&
@@ -215,7 +216,13 @@ const TokenList = (props: Props) => {
         }
 
         // Exclude wormhole-wrapped tokens with no balance
-        if (props.isSource && isWrappedToken(token, chain) && !balance) {
+        // unless it's canonical
+        if (
+          props.isSource &&
+          isWrappedToken(token, chain) &&
+          !isCanonicalToken(token, chain) &&
+          !balance
+        ) {
           return false;
         }
 
