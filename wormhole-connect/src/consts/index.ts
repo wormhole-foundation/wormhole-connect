a299d00c541010ad63a593fe899cbb56e35603b9
Artur Sapek <art@wormholelabs.xyz>
2024-09-26 13:14:50 -0400

    remove max CCTP size code to resolve TODO

--- wormhole-connect/src/consts/index.ts
+++ wormhole-connect/src/consts/index.ts
@@ -1,4 +1,2 @@
 export const GOVERNOR_WHITEPAPER_URL =
   'https://github.com/wormhole-foundation/wormhole/blob/main/whitepapers/0007_governor.md';
-
-export const CCTP_MAX_TRANSFER_LIMIT = 1000000;
