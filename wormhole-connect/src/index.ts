9548507ca68dfd249bf84057889dc61553b17b5f
Artur Sapek <art@wormholelabs.xyz>
2024-10-02 10:56:43 -0400

    export buildConfig for external config validation (#2788)

--- wormhole-connect/src/index.ts
+++ wormhole-connect/src/index.ts
@@ -9,6 +9,8 @@ import { dark, light } from './theme';
 
 import MAINNET from './config/mainnet';
 import TESTNET from './config/testnet';
+import { buildConfig } from './config';
+import type { WormholeConnectConfig } from './config/types';
 
 // Routes
 import { DEFAULT_ROUTES, nttRoutes } from './routes/operator';
@@ -24,7 +26,6 @@ import {
   nttManualRoute,
 } from '@wormhole-foundation/sdk-route-ntt';
 
-import type { WormholeConnectConfig } from './config/types';
 import type { Chain } from '@wormhole-foundation/sdk';
 
 import { wormholeConnectHosted } from './hosted';
@@ -40,8 +41,10 @@ const {
 export default WormholeConnect;
 
 export {
+  // Config related exports
   MAINNET,
   TESTNET,
+  buildConfig,
 
   // Types
   WormholeConnectConfig,
