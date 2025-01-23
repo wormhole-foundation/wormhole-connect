5eb4794cc90430a0cbfa59cf0f224496d40bb637
Emre Bogazliyanlioglu <emre@wormholelabs.xyz>
2025-01-23 17:32:47 +0300

    Add Arb check for source chain in the route
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>

--- wormhole-connect/src/routes/hyperliquid/index.ts
+++ wormhole-connect/src/routes/hyperliquid/index.ts
@@ -122,9 +122,23 @@ export class HyperliquidRoute<N extends Network>
     quote: Q,
     to: ChainAddress,
   ): Promise<R> {
+    // Check min amount requirement for Hyperliquid
     if (quote.details!.minReceived < 5) {
       throw new Error('Hyperliquid requires a minimum of 5 USDC to deposit');
     }
+
+    // If source chain is already Arbitrum, we can simply return a completed receipt and move to manual claim
+    if (request.fromChain.chain === 'Arbitrum') {
+      return {
+        attestation: { id: undefined },
+        originTxs: [],
+        destinationTxs: [],
+        from: 'Arbitrum',
+        to: 'Arbitrum',
+        state: TransferState.DestinationFinalized,
+      };
+    }
+
     return this.mayanRoute.initiate(request, signer, quote, to);
   }
 
