5571c4df538a4b623271f070de25d673fa99ddbf
Emre Bogazliyanlioglu <emre@wormholelabs.xyz>
2025-04-21 00:12:04 +0300

    Use SDK for contrack addresses
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>

--- wormhole-connect/tests/e2e/config/constants.ts
+++ wormhole-connect/tests/e2e/config/constants.ts
@@ -1,14 +1,16 @@
+import { circle } from '@wormhole-foundation/sdk';
+
 export const CONTRACTS = {
   Arbitrum: {
-    USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+    USDC: circle.usdcContract.get('Mainnet', 'Arbitrum'),
   },
   Base: {
-    USDC: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    USDC: circle.usdcContract.get('Mainnet', 'Base'),
   },
   Ethereum: {
-    USDC: '0xA0b86991c6218B36c1d19D4a2e9EB0CE3606EB48',
+    USDC: circle.usdcContract.get('Mainnet', 'Ethereum'),
   },
   Solana: {
-    USDC: '9n4nbM75f5Ui33ZbPYXn8c3yY2g6v7d2e1j6h8k3x3z4',
+    USDC: circle.usdcContract.get('Mainnet', 'Solana'),
   },
 };
