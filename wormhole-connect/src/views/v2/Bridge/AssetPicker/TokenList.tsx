465bdcdebacb5424e2522d4b78ead78bd7ba7b97
Artur Sapek <art@wormholelabs.xyz>
2025-05-09 12:43:12 -0400

    ?

--- wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenList.tsx
+++ wormhole-connect/src/views/v2/Bridge/AssetPicker/TokenList.tsx
@@ -263,7 +263,7 @@ const TokenList = (props: Props) => {
 
     if (props.isSource) {
       sorted = sorted.filter((t) => {
-        const bal = balances[tokenKey(t)].balance;
+        const bal = balances[tokenKey(t)]?.balance;
         return bal && sdkAmount.units(bal) > 0;
       });
     }
