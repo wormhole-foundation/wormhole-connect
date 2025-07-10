f00578abe5b29cdd0db0cfe475c4e9ee72e6fcf8
Artur Sapek <art@wormholelabs.xyz>
2025-07-10 10:16:45 -0400

    remove deprecated chains, simplify dependencies

--- src/config/devnet/chains.ts
+++ src/config/devnet/chains.ts
@@ -9,28 +9,4 @@ export const DEVNET_CHAINS: ChainsConfig = {
     icon: 'Ethereum',
     symbol: 'ETH',
   },
-  Osmosis: {
-    displayName: 'Osmosis',
-    sdkName: 'Osmosis',
-    explorerUrl: '',
-    explorerName: '',
-    icon: 'Osmosis',
-    symbol: 'OSMO',
-  },
-  Wormchain: {
-    sdkName: 'Wormchain',
-    displayName: 'Wormchain',
-    explorerUrl: '',
-    explorerName: '',
-    icon: 'Osmosis',
-    symbol: 'OSMO',
-  },
-  Terra2: {
-    sdkName: 'Terra2',
-    displayName: 'Terra',
-    explorerUrl: '',
-    explorerName: '',
-    icon: 'Terra2',
-    symbol: 'OSMO',
-  },
 };
