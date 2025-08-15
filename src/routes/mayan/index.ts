f09d96bf5b441d9a6e81186858d2a07f5f98abd4
Brad playdon <bplaydon@gmail.com>
2025-08-15 21:54:05 +0100

    refactor: folder structure to remove circular dependency

--- src/routes/mayan/index.ts
+++ src/routes/mayan/index.ts
@@ -1,6 +1,6 @@
-export { default as createMayanRouteWithReferrerFee } from '../../utils/mayan/utils';
 export { default as MayanRoute } from './MayanRoute';
 export { default as MayanRouteMCTP } from './MayanRouteMCTP';
 export { default as MayanRouteMONOCHAIN } from './MayanRouteMONOCHAIN';
 export { default as MayanRouteSWIFT } from './MayanRouteSWIFT';
 export { default as MayanRouteWH } from './MayanRouteWH';
+export { default as createMayanRouteWithReferrerFee } from './utils';
