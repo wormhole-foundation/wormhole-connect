30c0729d071667d54fdc14be6439bdcf3d1347ff
Kevin Peters <kevin@wormholelabs.xyz>
2025-08-18 08:08:10 -0500

    fix: remove unused import

--- src/contexts/wallet/WalletContext.test.tsx
+++ src/contexts/wallet/WalletContext.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { renderHook, act, waitFor } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useContext } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
