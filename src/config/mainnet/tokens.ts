df05dceb57fc6c9df0b49769bea1d55f1d65ea8c
Priyajeet Hora <priyajeet@gmail.com>
2025-08-27 16:25:08 -0700

    fix: update ts-sdk version

--- src/config/mainnet/tokens.ts
+++ src/config/mainnet/tokens.ts
@@ -754,6 +754,15 @@ export const MAINNET_TOKENS: TokenConfig[] = [
     symbol: 'XRP',
     tokenId: {
       chain: 'XRPLEVM',
+      address: 'native',
+    },
+    decimals: 18,
+    icon: TokenIcon.XRP,
+  },
+  {
+    symbol: 'WXRP',
+    tokenId: {
+      chain: 'XRPLEVM',
       address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
     },
     decimals: 18,
--- src/config/testnet/tokens.ts
+++ src/config/testnet/tokens.ts
@@ -417,6 +417,15 @@ export const TESTNET_TOKENS: TokenConfig[] = [
     symbol: 'XRP',
     tokenId: {
       chain: 'XRPLEVM',
+      address: 'native',
+    },
+    decimals: 18,
+    icon: TokenIcon.XRP,
+  },
+  {
+    symbol: 'WXRP',
+    tokenId: {
+      chain: 'XRPLEVM',
       address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
     },
     decimals: 18,
