3981853e848fdf1e81b28cd32ccd479915a3e797
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2025-10-03 21:55:26 +0300

    fix: move to function style definition

--- src/utils/whScanUtils.ts
+++ src/utils/whScanUtils.ts
@@ -1,12 +1,12 @@
 import type { WormholeScanTransaction } from 'hooks/useTransactionHistoryWHScan';
 
-export const isPortalBridgeAttestationTx = (
+export function isPortalBridgeAttestationTx(
   tx: WormholeScanTransaction,
-): boolean => {
+): boolean {
   /**
    * Attestation txs on PORTAL_TOKEN_BRIDGE can be identified by having a payloadType=2
    */
   return (
     'payloadType' in tx.content.payload && tx.content.payload.payloadType === 2
   );
-};
+}
