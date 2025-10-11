6e5f48c026ea4480f997d09bc403d269e3f36ddf
Emre Bogazliyanlioglu <emreboga@users.noreply.github.com>
2025-10-12 00:06:34 +0300

    feat: Center asset picker modal vertically (#3860)
    * feat: Center asset picker modal vertically
    
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>
    
    * fix: Make sure switching to More search view keeps the same height
    
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>
    
    ---------
    
    Signed-off-by: Emre Bogazliyanlioglu <emre@wormholelabs.xyz>

--- src/views/v3/Bridge/AssetPicker/PickerModal.tsx
+++ src/views/v3/Bridge/AssetPicker/PickerModal.tsx
@@ -64,15 +64,7 @@ function AssetPickerPopover({
     <Popover
       {...bindPopover(popupState)}
       transitionDuration={200}
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'center',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'center',
-      }}
+      anchorReference="none"
       marginThreshold={4}
       slotProps={{
         paper: {
@@ -88,6 +80,9 @@ function AssetPickerPopover({
         },
         root: {
           sx: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
             backdropFilter: 'blur(4px)',
             WebkitBackdropFilter: 'blur(4px)', // Safari support
           },
