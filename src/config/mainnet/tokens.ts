a07419d7ef30bb5b65b7214ebbacd1ac4cbc9787
Dave Bosley <DBosley@users.noreply.github.com>
2025-10-13 17:41:16 -0600

    fix: remove WCTC token configuration from mainnet tokens list (#3867)

--- src/config/mainnet/tokens.ts
+++ src/config/mainnet/tokens.ts
@@ -63,15 +63,6 @@ export const MAINNET_TOKENS: TokenConfig[] = [
     },
   },
   {
-    symbol: 'WCTC',
-    decimals: 18,
-    icon: TokenIcon.CTC,
-    tokenId: {
-      chain: 'Ethereum',
-      address: '0xaafec8e08d524d534327fa13fb306f440b5f88eb',
-    },
-  },
-  {
     symbol: 'POL',
     decimals: 18,
     icon: TokenIcon.POLYGON,
