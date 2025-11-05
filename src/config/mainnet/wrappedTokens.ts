2fe3351c73d070e8a99e8df21e970b7d21a4fd96
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2025-11-05 08:56:02 -0600

    chore: add Fogo WSOL wrapped address (#3906)

--- src/config/mainnet/wrappedTokens.ts
+++ src/config/mainnet/wrappedTokens.ts
@@ -366,6 +366,7 @@ export const MAINNET_WRAPPED_TOKENS = {
       Scroll: '0xCDf95E1F720caade4b1DC83ABfE15400D2a458AD',
       Worldchain: '0xEfae32D1c15EDBaEA3ebdDe1e2C51003AED04d30',
       Unichain: '0xbdE8A5331E8Ac4831cf8ea9e42e229219EafaB97',
+      Fogo: 'HLc5hqihQGFU68488j7HkdyF6rywyJfV46BN6Dn8W5ug',
     },
     EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v: {
       Ethereum: '0x41f7B8b9b897276b7AAE926a9016935280b44E97',
