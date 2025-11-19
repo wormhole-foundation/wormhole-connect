9d86067700fc2294b47bd084e250469fe03b99e7
Priyajeet Hora <priyajeet@gmail.com>
2025-11-18 19:06:26 -0800

    feat: add new flags to disable token selectors
    - `disableSourceAssetSelector` disables the source token selector
    - `disableDestinationAssetSelector` disables the destination token selector

--- src/config/ui.ts
+++ src/config/ui.ts
@@ -63,6 +63,12 @@ export type UiConfig = {
 
   // Filter transaction history by specific chains
   transactionHistoryChains?: Chain[];
+
+  // When enabled disables the source asset selector button
+  disableSourceAssetSelector?: boolean;
+
+  // When enabled disables the destination asset selector button
+  disableDestinationAssetSelector?: boolean;
 };
 
 export type TestOptions = {
---
+++ src/views/v3/Bridge/AssetPicker/TokenPickerButton.tsx
@@ -0,0 +1,114 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
+import config from 'config';
+import type { ChainConfig } from 'config/types';
+import type { Token } from 'config/tokens';
+import AssetBadge from 'components/AssetBadge';
+import TokenPickerButtonContent from './TokenPickerButtonContent';
+import { getTokenDisplaySymbolByTokenAddress } from 'utils';
+import React from 'react';
+import type { bindTrigger } from 'material-ui-popup-state/hooks';
+
+interface TokenPickerButtonProps {
+  chainConfig: ChainConfig | undefined;
+  dataTestId?: string;
+  isSource: boolean;
+  isTransactionInProgress: boolean;
+  openDrawer: () => void;
+  token: Token | undefined;
+  triggerProps: ReturnType<typeof bindTrigger>;
+}
+
+function TokenPickerButton({
+  chainConfig,
+  dataTestId,
+  isSource,
+  isTransactionInProgress,
+  openDrawer,
+  token,
+  triggerProps,
+}: TokenPickerButtonProps) {
+  const isEnabled = isSource
+    ? !config.ui.disableSourceAssetSelector
+    : !config.ui.disableDestinationAssetSelector;
+
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const symbol = token ? getTokenDisplaySymbolByTokenAddress(token) : '';
+  const label = isSource ? 'Select source asset' : 'Select destination asset';
+  const isInteractable = isEnabled && !isTransactionInProgress;
+  const eventHandlers = mobile ? { onClick: openDrawer } : triggerProps;
+  const triggers = isInteractable ? eventHandlers : undefined;
+  const role = isInteractable ? 'button' : undefined;
+  const ariaLabel = isInteractable ? label : undefined;
+
+  const styles = React.useMemo(
+    () => ({
+      selector: {
+        cursor: isInteractable ? 'pointer' : 'default',
+        borderRadius: '48px',
+        border: `1px solid ${theme.palette.input.border}`,
+        background: theme.palette.input.background,
+        minWidth: '120px',
+        height: '48px',
+        '&:hover': {
+          borderColor: isInteractable
+            ? theme.palette.primary.main
+            : theme.palette.input.border,
+        },
+      },
+      disabled: {
+        opacity: '0.6',
+        cursor: 'default',
+        pointerEvents: 'none',
+      },
+      cardContent: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: '8px',
+        paddingRight: '12px',
+        paddingTop: '6px',
+        paddingBottom: '6px',
+        ':last-child': {
+          paddingLeft: '8px',
+          paddingRight: '12px',
+          paddingTop: '6px',
+          paddingBottom: '6px',
+        },
+      },
+      chainSelector: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      },
+    }),
+    [theme, isInteractable],
+  );
+
+  return (
+    <Card
+      sx={[styles.selector, !isInteractable && styles.disabled]}
+      data-testid={dataTestId}
+      role={role}
+      aria-label={ariaLabel}
+      variant="elevation"
+      {...triggers}
+    >
+      <CardContent sx={styles.cardContent}>
+        <Typography sx={styles.chainSelector} component="div" gap={1}>
+          <AssetBadge chainConfig={chainConfig} token={token} />
+          <TokenPickerButtonContent
+            symbol={symbol}
+            isSelectable={isInteractable}
+          />
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default React.memo(TokenPickerButton);
