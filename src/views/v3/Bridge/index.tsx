40ba43e3ecebe62f95db49b16f94b9abf9fdc1f7
Kevin Peters <kevin@wormholelabs.xyz>
2025-11-21 11:54:08 -0600

    Revert "feat: disable auto gas dropoff (#3907)"
    This reverts commit 066e2ebb292273773e78847b5bd17f18878d9d17.

--- src/views/v3/Bridge/index.tsx
+++ src/views/v3/Bridge/index.tsx
@@ -31,6 +31,7 @@ import { useGetTokens } from 'hooks/useGetTokens';
 import useGetTokenBalances from 'hooks/useGetTokenBalances';
 import { useWalletCompatibility } from 'hooks/useWalletCompatibility';
 import { useConnectToLastUsedWallet } from 'hooks/useConnectToLastUsedWallet';
+import { useAutoEnableGasDropOff } from 'hooks/useAutoEnableGasDropoff';
 import PoweredByIcon from 'icons/PoweredBy';
 import type { RootState } from 'store';
 import {
@@ -66,6 +67,8 @@ function Bridge(props: BridgeProps) {
   const dispatch = useDispatch();
 
   const [showHistory, setShowHistory] = useState(props.showHistory ?? false);
+  const [hasUserManuallyChangedGas, setHasUserManuallyChangedGas] =
+    useState(false);
 
   const { lastTokenCacheUpdate } = useTokens();
   const [errorCopied, setErrorCopied] = useState(false);
@@ -268,6 +271,23 @@ function Bridge(props: BridgeProps) {
     destination: destBalanceRequest,
   });
 
+  // Reset manual gas change flag on destination chain change
+  useEffect(() => {
+    setHasUserManuallyChangedGas(false);
+  }, [destChain]);
+
+  // Auto-set nativeGas when destination native balance is zero
+  // and user has not manually changed gas setting
+  useAutoEnableGasDropOff({
+    route,
+    destChain,
+    receivingWalletAddress: receivingWallet?.address,
+    destinationBalances: balances.destination.balances,
+    hasUserManuallyChangedGas,
+    currentToNativeToken: toNativeToken,
+    isFetchingBalances: balances.isFetching,
+  });
+
   // Validate amount
   const amountValidation = useAmountValidation({
     balance: sourceToken
@@ -342,6 +362,11 @@ function Bridge(props: BridgeProps) {
     },
     [dispatch],
   );
+
+  // Handler for manual gas changes
+  const handleManualGasChange = useCallback(() => {
+    setHasUserManuallyChangedGas(true);
+  }, []);
 
   // Determine which wallet connector to show
   const walletConnectorProps = useMemo(() => {
@@ -593,6 +618,7 @@ function Bridge(props: BridgeProps) {
               onRouteChange={handleRouteChange}
               quotes={quotes}
               isLoading={isFetchingQuotes}
+              onManualGasChange={handleManualGasChange}
             />
           )}
         </Box>
