4e27504e5dd8d48bcc2a20ed95f58f0a9e01971b
Priyajeet Hora <priyajeet@users.noreply.github.com>
2026-01-20 12:26:59 -0800

    fix: optimize mayan route to remove unnecessary quoting (#4008)
    - Should not request mono chain quotes when not same chain swaps
    - Should not request cross chain quotes when same chain swaps

--- src/routes/mayan/MayanRouteMCTP.ts
+++ src/routes/mayan/MayanRouteMCTP.ts
@@ -1,10 +1,10 @@
 import type { Network } from '@wormhole-foundation/sdk-base';
 import type { routes } from '@wormhole-foundation/sdk-connect';
-import { MayanRouteBase } from './MayanRouteBase';
+import { MayanRouteCrossChain } from './MayanRouteCrossChain';
 import { MayanProtocol } from './types';
 
 export class MayanRouteMCTP<N extends Network>
-  extends MayanRouteBase<N>
+  extends MayanRouteCrossChain<N>
   implements routes.StaticRouteMethods<typeof MayanRouteMCTP>
 {
   static meta = {
--- src/routes/mayan/MayanRouteSWIFT.ts
+++ src/routes/mayan/MayanRouteSWIFT.ts
@@ -1,10 +1,10 @@
 import type { Network } from '@wormhole-foundation/sdk-base';
 import type { routes } from '@wormhole-foundation/sdk-connect';
-import { MayanRouteBase } from './MayanRouteBase';
+import { MayanRouteCrossChain } from './MayanRouteCrossChain';
 import { MayanProtocol } from './types';
 
 export class MayanRouteSWIFT<N extends Network>
-  extends MayanRouteBase<N>
+  extends MayanRouteCrossChain<N>
   implements routes.StaticRouteMethods<typeof MayanRouteSWIFT>
 {
   static meta = {
